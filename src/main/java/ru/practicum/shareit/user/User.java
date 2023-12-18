23041306ec621127fec3e3e8406ea068a2ea0cbb
AlexanderTsybulskiy <kaktus3333@yandex.ru>
2023-12-18 15:41:22 +0300

    Рефакторинг кода (применение ORM)

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -4,14 +4,35 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private long id;
+    @Column(name = "first_name", nullable = false)
     private String name;
+    @Column(name = "last_name")
+    private String lastName;
     private String email;
+    @Column(name = "registration_date")
+    private LocalDateTime registrationDate = LocalDateTime.now();
+    @Enumerated(EnumType.STRING)
+    private UserState state;
 
     public User(long id, String name, String email) {
         this.id = id;
